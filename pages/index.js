383dcfa672515065c232e67073fdf7f15f68f067
irsanrais <irsanrais94@gmail.com>
2023-07-31 08:52:40 +0700

    add route

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { getFeaturedEvents } from "../helpers/api-utils";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 
 const HomePage = (props) => {
   return (
     <div>
+      <Head>
+        <title>NextJs Events</title>
+      </Head>
       <EventList items={props.events} />
     </div>
   );
